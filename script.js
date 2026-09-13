71cf2b54052a0ef943351f606b2ec475661a4eeb
siemgebreyohanes22-debug <siemgebreyohanes22@gmail.com>
2026-09-12 21:29:05 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// Smooth scrolling for navigation links
 
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+
+    link.addEventListener("click", function(event) {
+
+        event.preventDefault();
+
+        const section = document.querySelector(this.getAttribute("href"));
+
+        section.scrollIntoView({
+            behavior: "smooth"
+        });
+
+    });
+
+});
